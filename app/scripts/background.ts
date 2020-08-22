ee7df00ae54eafc4eceba8eef3be8679b15c4b85
kkito <kkitokkito@126.com>
2020-08-22 10:15:54 +0800

    只有特定的站点才有效

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -2,7 +2,22 @@
 // import 'chromereload/devonly'
 
 chrome.runtime.onInstalled.addListener((details) => {
-  console.log('previousVersion', details.previousVersion);
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'google' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
 });
 
-console.log(`'Allo 'Allo! Event Page`);
+console.log(`'Allo 'Allo! Event Page2`);
+
