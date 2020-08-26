68f063419c5c50d71b909403a7598e4d352682e6
kkito <kkitokkito@126.com>
2020-08-26 09:17:11 +0800

    mardown show

--- app/scripts/options.ts
+++ app/scripts/options.ts
@@ -10,6 +10,50 @@ try {
   console.log('err');
   console.log(e);
 }
+
+function updateMarkdown2Html(markDown: string): boolean {
+  const regex = /(<([^>]+)>)/ig;
+  markDown = markDown.replace(regex, '\n');
+
+  const converter = new showdown.Converter(),
+  html      = converter.makeHtml(markDown);
+  console.log(markDown);
+  console.log(html);
+  const ele = document.getElementById('markdown');
+  if (ele) {
+    ele.innerHTML = html;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const execFun = function() {
+  const matches = document.querySelectorAll('div[contenteditable="true"]');
+  if (matches.length === 1) {
+    return matches[0].innerHTML;
+  } else {
+    return '';
+  }
+};
+
+function execMarkdown() {
+  chrome.tabs.executeScript(
+    {
+      code: `(${execFun.toString()})()`,
+    },
+    (results) => {
+      // Here we have just the innerHTML and not DOM structure
+      console.log('Popup script:');
+      if (results.length === 1) {
+        updateMarkdown2Html(results[0]);
+      }
+    }
+  );
+}
+
+setTimeout(execMarkdown, 100);
+
 if (ele) {
   ele.addEventListener('click', () => {
     console.log('Popup DOM fully loaded and parsed');
@@ -20,23 +64,7 @@ if (ele) {
     //     console.log(document.body);
     //     return document.body.innerHTML;
     // }
-
-    const execFun = function() {
-      const matches = document.querySelectorAll('div[contenteditable="true"]');
-      if (matches.length === 1) {
-        return matches[0].innerHTML;
-      } else {
-        return '';
-      }
-    };
-
     // //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
-    chrome.tabs.executeScript({
-        code: `(${execFun.toString()})()`
-    }, (results) => {
-        // Here we have just the innerHTML and not DOM structure
-        console.log('Popup script:');
-        console.log(results);
-    });
+    execMarkdown();
   });
 }
