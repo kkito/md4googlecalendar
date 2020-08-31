f4c9ec0adbb2bf2c29f14ddb9293f4fd6be28e90
kkito <kkitokkito@126.com>
2020-08-31 09:15:49 +0800

    内嵌内容

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,4 +1,81 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-console.log(`'Allo 'Allo! Content script`);
+console.log(`============= inited md4google calendar`);
+
+class MD4Gcalendar {
+  readonly markDownDivId = 'md4g_markdown';
+  protected calledTimes = 0;
+  protected initedMardownDiv = false;
+  protected lastMarkdown = ``;
+
+  public intervalCall() {
+    this.calledTimes++;
+    console.log(`interval called ${this.calledTimes}`);
+    if (this.hasEditorInPage()) {
+      const mdDiv = this.getMardDownDiv();
+      console.log(`inited!!!`);
+      const mdc = this.getMarkdownContent();
+      if (mdc !== this.lastMarkdown) {
+        this.lastMarkdown = mdc;
+        this.updateMarkdown2Html(mdc);
+      }
+    }
+  }
+
+  public hasEditorInPage() {
+    const ele = this.getOriginEditorEle();
+    return !!ele;
+  }
+
+  public updateMarkdown2Html(markDown: string) {
+    const converter = new showdown.Converter(),
+      html = converter.makeHtml(markDown);
+    console.log(markDown);
+    console.log(html);
+    this.getMardDownDiv()!.innerHTML = html;
+  }
+
+  protected getMarkdownContent() {
+    const markDown = this.getOriginEditorEle()!.innerHTML;
+    const regex = /(<([^>]+)>)/gi;
+    return markDown.replace(regex, '\n').replace(`    `, `\t`);
+  }
+
+  protected getOriginEditorEle() {
+    const matches = document.querySelectorAll('div[contenteditable="true"]');
+    if (matches.length === 1) {
+      return matches[0];
+    } else {
+      return null;
+    }
+  }
+
+  protected getMardDownDiv() {
+    if (!this.initedMardownDiv) {
+      const div = document.createElement(`div`);
+      div.setAttribute(`id`, this.markDownDivId);
+      div.setAttribute(`class`, `markdown-body`);
+      const editor = this.getOriginEditorEle();
+      const rect = editor!.getBoundingClientRect();
+      div.setAttribute(
+        `style`,
+        `position:absolute;top:${rect.top}px;left:${
+          rect.left + rect.width + 80
+        }px;`
+      );
+      div.innerHTML = `not inited!!!!`;
+      document.getElementsByTagName(`body`)[0].appendChild(div);
+      this.initedMardownDiv = true;
+    }
+    return document.getElementById(this.markDownDivId);
+  }
+}
+
+const md4g = new MD4Gcalendar();
+
+setInterval(() => {
+  md4g.intervalCall();
+}, 3000);
+
+console.log(`'Allo 'Allo! Content script 234`);
